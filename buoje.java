89a0f4bf60201b079a904dfd1817aecb1a21d0a9
Zakky Aulia Aldrin <zakkyaulialdrin@gmail.com>
2025-06-15 22:23:01 +0700

    bapak kau

---
+++ buoje.java
@@ -0,0 +1,5 @@
+public static void main(String[] args) {
+    // This is a placeholder for the main method.
+    // You can add your code logic here.
+    System.out.println("Hello, World!");        
+}
